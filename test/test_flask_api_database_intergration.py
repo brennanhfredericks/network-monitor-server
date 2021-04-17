69df1b9441e229d23ef0739b12bca8a2b7360d71
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-04-17 13:19:48 +0200

    removed test data files

--- test/test_flask_api_database_intergration.py
+++ test/test_flask_api_database_intergration.py
@@ -2,6 +2,12 @@ import pymysql
 from connection import get_connection
 
 
+def get_submitter_service_data():
+    """
+    - post process data that was stored locally by the submission service
+    """
+
+
 def count_packets_in_db():
     # connection to database
 
@@ -21,3 +27,10 @@ def count_packets_in_db():
 def test_count_packets_in_db():
 
     count_packets_in_db()
+
+
+def post_packets_flask_api_and_compare_db():
+    """
+    - only comparing numbers
+    - TODO implement value comparision
+    """
