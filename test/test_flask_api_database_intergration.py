337db296dd9b60e1e67f16110a137148d358fbac
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-04-17 15:22:20 +0200

    configured nginx and able to reach flask api

--- test/test_flask_api_database_intergration.py
+++ test/test_flask_api_database_intergration.py
@@ -1,11 +1,22 @@
 import pymysql
+import os
+import json
 from connection import get_connection
+from urllib import request, parse
 
 
 def get_submitter_service_data():
     """
     - post process data that was stored locally by the submission service
     """
+    base_path = "./test/data/submitter_service"
+    data_files = os.listdir(base_path)
+
+    for f in data_files:
+        file_path = os.path.join(base_path, f)
+        with open(file_path, "r") as fin:
+            for line in fin:
+                yield json.loads(line)
 
 
 def count_packets_in_db():
@@ -34,3 +45,20 @@ def post_packets_flask_api_and_compare_db():
     - only comparing numbers
     - TODO implement value comparision
     """
+    url = "http://localhost:5050/packets"
+    count_inserts = 0
+    for packet in get_submitter_service_data():
+
+        d = json.dumps(packet).encode("utf-8")
+
+        req = request.Request(url, data=d, method="POST")
+
+        resp = request.urlopen(req)
+        print(resp)
+        break
+
+
+def test_post_packets_flask_api_and_compare_db():
+
+    post_packets_flask_api_and_compare_db()
+    assert False
