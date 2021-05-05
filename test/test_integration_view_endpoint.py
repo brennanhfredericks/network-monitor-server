e6b2a0e746481bd9e3991663d60fd1ea11ce751d
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-05-05 16:37:22 +0200

    docker can't see host error

--- test/test_integration_view_endpoint.py
+++ test/test_integration_view_endpoint.py
@@ -1,20 +1,29 @@
 import os
 import json
-from urllib import request
-import urllib
+from urllib import request, parse
 
-url_local = "http://localhost:5050"
-url = "http://10.0.0.10:5050"
+
+url_local = "http://localhost:5050/"
 
 
 def packets_tables_all_view_points():
-    params = urllib.urlencode({"protocolname": "packet", "limit": 0})
-    req = request.Request(url + "packets/tables/view?" + params)
-    resp = request.urlopen(req)
 
+    req = request.Request(
+        "http://localhost:5050/packets/tables/views?protocolname=ipv4&limit=10",
+        data=None,
+        headers={
+            "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
+        },
+    )
+
+    resp = request.urlopen(req)
     assert resp.getcode() == 200
 
 
 def test_packets_tables_all_view_points():
-    # packets_tables_all_view_points()
-    ...
+    packets_tables_all_view_points()
+
+
+if __name__ == "__main__":
+
+    test_packets_tables_all_view_points()
